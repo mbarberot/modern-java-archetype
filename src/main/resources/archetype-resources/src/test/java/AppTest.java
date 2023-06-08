121f2b31e082cafb683a4f2419a3f2d49d7e4f9b
Mathieu Barberot <mathieu.barberot@gmail.com>
2023-06-08 23:35:14 +0200

    feat: setup with lombok, junit-assertj-mockito-datafaker, archunit, errorprone-nullaway

---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,13 @@
+package $package;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertThat(true).isTrue();
+    }
+}
