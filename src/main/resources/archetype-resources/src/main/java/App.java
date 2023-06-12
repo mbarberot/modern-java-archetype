b3daa11f047f3cc3cc82bddccb4cb8081fb5c171
Mathieu Barberot <mathieu.barberot@gmail.com>
2023-06-12 21:53:48 +0200

    feat: add logger (slf4j + log4j2)

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,11 +1,20 @@
 package $package;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
+
+    private static Logger log = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 
     public String fizzbuzz(Integer i) {
+
+        log.info("Running Fizzbuzz with {}" i);
+
         if (i % 15 == 0) {
             return "FizzBuzz";
         }
